c2a8fb225c90431016419a93f008fb604d53da9a
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-12-31 08:40:37 +0000

    x

--- MatroskaConsole/Program.cs
+++ MatroskaConsole/Program.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
+using ATL;
+using Commons;
 using CSCore;
 using CSCore.Codecs.OPUS;
 using CSCore.SoundOut;
@@ -185,6 +187,139 @@ namespace MatroskaConsole
         }
     }
 
+    class OpusHeader
+    {
+        public String ID;
+        public byte Version;
+        public byte OutputChannelCount;
+        public UInt16 PreSkip;
+        public UInt32 InputSampleRate;
+        public Int16 OutputGain;
+        public byte ChannelMappingFamily;
+
+        public byte StreamCount;
+        public byte CoupledStreamCount;
+        public byte[] ChannelMapping;
+
+        public void Reset()
+        {
+            ID = "";
+            Version = 0;
+            OutputChannelCount = 0;
+            PreSkip = 0;
+            InputSampleRate = 0;
+            OutputGain = 0;
+            ChannelMappingFamily = 0;
+            StreamCount = 0;
+            CoupledStreamCount = 0;
+        }
+    }
+
+    class OggHeader
+    {
+        // Ogg page header ID
+        const String OGG_PAGE_ID = "OggS";
+
+        // Vorbis identification packet (frame) ID
+        static readonly String VORBIS_HEADER_ID = (char)1 + "vorbis";
+
+        // Vorbis tag packet (frame) ID
+        static readonly String VORBIS_TAG_ID = (char)3 + "vorbis";
+
+        // Vorbis setup packet (frame) ID
+        static readonly String VORBIS_SETUP_ID = (char)5 + "vorbis";
+
+        // Vorbis parameter frame ID
+        const String OPUS_HEADER_ID = "OpusHead";
+
+        // Opus tag frame ID
+        const String OPUS_TAG_ID = "OpusTags";
+
+
+        public string ID;                                               // Always "OggS"
+        public byte StreamVersion;                           // Stream structure version
+        public byte TypeFlag;                                        // Header type flag
+        public ulong AbsolutePosition;                      // Absolute granule position
+        public int Serial;                                       // Stream serial number
+        public int PageNumber;                                   // Page sequence number
+        public uint Checksum;                                              // Page CRC32
+        public byte Segments;                                 // Number of page segments
+        public byte[] LacingValues;                     // Lacing values - segment sizes
+
+        public void Reset()
+        {
+            ID = "";
+            StreamVersion = 0;
+            TypeFlag = 0;
+            AbsolutePosition = 0;
+            Serial = 0;
+            PageNumber = 0;
+            Checksum = 0;
+            Segments = 0;
+        }
+
+        public void ReadFromStream(BufferedBinaryReader r)
+        {
+            ID = Utils.Latin1Encoding.GetString(r.ReadBytes(4));
+            StreamVersion = r.ReadByte();
+            TypeFlag = r.ReadByte();
+            AbsolutePosition = r.ReadUInt64();
+            Serial = r.ReadInt32();
+            PageNumber = r.ReadInt32();
+            Checksum = r.ReadUInt32();
+            Segments = r.ReadByte();
+            LacingValues = r.ReadBytes(Segments);
+        }
+
+        public void ReadFromStream(BinaryReader r)
+        {
+            ID = Utils.Latin1Encoding.GetString(r.ReadBytes(4));
+            StreamVersion = r.ReadByte();
+            TypeFlag = r.ReadByte();
+            AbsolutePosition = r.ReadUInt64();
+            Serial = r.ReadInt32();
+            PageNumber = r.ReadInt32();
+            Checksum = r.ReadUInt32();
+            Segments = r.ReadByte();
+            LacingValues = r.ReadBytes(Segments);
+        }
+
+        public void WriteToStream(BinaryWriter w)
+        {
+            w.Write(Utils.Latin1Encoding.GetBytes(ID));
+            w.Write(StreamVersion);
+            w.Write(TypeFlag);
+            w.Write(AbsolutePosition);
+            w.Write(Serial);
+            w.Write(PageNumber);
+            w.Write(Checksum);
+            w.Write(Segments);
+            w.Write(LacingValues);
+        }
+
+        public int GetPageLength()
+        {
+            int length = 0;
+            for (int i = 0; i < Segments; i++)
+            {
+                length += LacingValues[i];
+            }
+            return length;
+        }
+
+        public int GetHeaderSize()
+        {
+            return 27 + LacingValues.Length;
+        }
+
+        public bool IsValid()
+        {
+            return ((ID != null) && ID.Equals(OGG_PAGE_ID));
+        }
+    }
+
+    
+
     class Program
     {
         static async Task Main(string[] args)
@@ -245,7 +380,25 @@ namespace MatroskaConsole
             br.Flush();
 
             ms1 = new MemoryStream();
-            var org = File.ReadAllBytes(@"C:\Users\azurestef\Downloads\Estas Tonne - Internal Flight Experience (Live in Cluj Napoca)_track1_[eng]_DELAY 0ms.opus");
+            var org = File.OpenRead(@"C:\Users\azurestef\Downloads\Estas Tonne - Internal Flight Experience (Live in Cluj Napoca)_track1_[eng]_DELAY 0ms.opus");
+
+            var oggHeader1 = new OggHeader();
+            var source = new BufferedBinaryReader(org);
+            oggHeader1.ReadFromStream(source);
+
+
+            var ID = Utils.Latin1Encoding.GetString(source.ReadBytes(8));
+            //var isValidHeader = OPUS_HEADER_ID.Equals(info.OpusParameters.ID);
+
+            var Version = source.ReadByte();
+            var OutputChannelCount = source.ReadByte();
+            var PreSkip = source.ReadUInt16();
+            //info.OpusParameters.InputSampleRate = source.ReadUInt32();
+            var InputSampleRate = 48000; // Actual sample rate is hardware-dependent. Let's assume for now that the hardware ATL runs on supports 48KHz
+            source.Seek(4, SeekOrigin.Current);
+            var OutputGain = source.ReadInt16();
+
+            var ChannelMappingFamily = source.ReadByte();
 
             // ms1.Write(org, 0, 123);
 
@@ -327,25 +480,27 @@ namespace MatroskaConsole
 
                                 //ms1.Write(buffer, 0, read);
 
-
+                                
 
                                 var memH = new MemoryStream();
                                 var oggHeader = new BinaryWriter(memH);
 
-                                // 4F	67	67	53	00	2	00	00	00	00	00	00	00	00	8E	2B	E00	BA	00	00	00	00
+                                oggHeader1.WriteToStream(oggHeader);
+
+                                //// 4F	67	67	53	00	2	00	00	00	00	00	00	00	00	8E	2B	E00	BA	00	00	00	00
 
 
-                                oggHeader.Write(System.Text.Encoding.ASCII.GetBytes("OggS"));
-                                oggHeader.Write((byte)0); // Header type – 8 bits
-                                oggHeader.Write((byte)0); // start
-                                oggHeader.Write((ulong)0); // gr
-                                oggHeader.Write(0x8E); // 8E 2B E0 BA 
-                                oggHeader.Write(0x2B); // 8E 2B E0 BA 
-                                oggHeader.Write(0xE0); // 8E 2B E0 BA 
-                                oggHeader.Write(0xBA); // 8E 2B E0 BA 
-                                oggHeader.Write(oggPacket); // packet
-                                oggHeader.Write(oggPacket); // crc
-                                oggHeader.Flush();
+                                //oggHeader.Write(System.Text.Encoding.ASCII.GetBytes("OggS"));
+                                //oggHeader.Write((byte)0); // Header type – 8 bits
+                                //oggHeader.Write((byte)0); // start
+                                //oggHeader.Write((ulong)0); // gr
+                                //oggHeader.Write(0x8E); // 8E 2B E0 BA 
+                                //oggHeader.Write(0x2B); // 8E 2B E0 BA 
+                                //oggHeader.Write(0xE0); // 8E 2B E0 BA 
+                                //oggHeader.Write(0xBA); // 8E 2B E0 BA 
+                                //oggHeader.Write(oggPacket); // packet
+                                //oggHeader.Write(oggPacket); // crc
+                                //oggHeader.Flush();
 
                                 memH.Position = 0;
                                 memH.CopyTo(ms1);
